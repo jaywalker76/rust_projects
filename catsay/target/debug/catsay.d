011e52ca6b8d199c3fa4fadad052c3cb3359a98a
Julio Costa <juliocosta1@gmail.com>
2020-03-12 21:10:25 +0000

    first project - catsay

---
+++ catsay/target/debug/catsay.d
@@ -0,0 +1,1 @@
+/home/julio/Documents/projects/rust_projects/catsay/target/debug/catsay: /home/julio/Documents/projects/rust_projects/catsay/src/main.rs
---
+++ catsay/target/debug/deps/catsay-6a4d32089e6dc08b.d
@@ -0,0 +1,5 @@
+/home/julio/Documents/projects/rust_projects/catsay/target/debug/deps/catsay-6a4d32089e6dc08b: src/main.rs
+
+/home/julio/Documents/projects/rust_projects/catsay/target/debug/deps/catsay-6a4d32089e6dc08b.d: src/main.rs
+
+src/main.rs:
